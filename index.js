70ae6ffcf0bf4008b692fa65113d2a41912283e7
Vamshi Krishna Motoori <vamshi.motoori@zee.com>
2021-03-31 13:41:33 +0530

    jhon

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 //import App from './src/App'
 //import App from './News/APP'
 //import App from './KIngNews/App'
-
+import App from './Jhon Wick/App'
 import Home from './src/Jhon Wick/Home'
 import {name as appName} from './app.json';
 
